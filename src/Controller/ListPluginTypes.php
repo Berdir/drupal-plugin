ceed991caca8f4999250ed5c0ffd09666b7beaef
Bart Feenstra <bart@mynameisbart.com>
2015-07-16 09:29:31 +0200

    Display human-readable labels for the 'core' provider.

--- src/Controller/ListPluginTypes.php
+++ src/Controller/ListPluginTypes.php
@@ -7,54 +7,12 @@
 
 namespace Drupal\plugin\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\plugin\PluginTypeInterface;
-use Drupal\plugin\PluginTypeManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Handles the "list plugin types" route.
  */
-class ListPluginTypes extends ControllerBase {
-
-  /**
-   * The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * The plugin type manager.
-   *
-   * @var \Drupal\plugin\PluginTypeManagerInterface
-   */
-  protected $pluginTypeManager;
-
-  /**
-   * Constructs a new class instance.
-   *
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
-   *   The string translator.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   * @param \Drupal\plugin\PluginTypeManagerInterface $plugin_type_manager
-   *   The plugin type manager.
-   */
-  public function __construct(TranslationInterface $string_translation, ModuleHandlerInterface $module_handler, PluginTypeManagerInterface $plugin_type_manager) {
-    $this->moduleHandler = $module_handler;
-    $this->pluginTypeManager = $plugin_type_manager;
-    $this->stringTranslation = $string_translation;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static($container->get('string_translation'), $container->get('module_handler'), $container->get('plugin.plugin_type_manager'));
-  }
+class ListPluginTypes extends ListBase {
 
   /**
    * Handles the route.
@@ -83,7 +41,7 @@ class ListPluginTypes extends ControllerBase {
         '#markup' => $plugin_type->getDescription(),
       ];
       $build[$plugin_type_id]['provider'] = [
-        '#markup' => $this->moduleHandler->getName($plugin_type->getProvider()),
+        '#markup' => $this->getProviderLabel($plugin_type->getProvider()),
       ];
       $build[$plugin_type_id]['operations'] = [
         '#links' => $operations,
--- src/Controller/ListPlugins.php
+++ src/Controller/ListPlugins.php
@@ -7,54 +7,12 @@
 
 namespace Drupal\plugin\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\StringTranslation\TranslationInterface;
-use Drupal\plugin\PluginTypeManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Handles the "list plugin" route.
  */
-class ListPlugins extends ControllerBase {
-
-  /**
-   * The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * The plugin type manager.
-   *
-   * @var \Drupal\plugin\PluginTypeManagerInterface
-   */
-  protected $pluginTypeManager;
-
-  /**
-   * Constructs a new class instance.
-   *
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
-   *   The string translator.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   * @param \Drupal\plugin\PluginTypeManagerInterface $plugin_type_manager
-   *   The plugin type manager.
-   */
-  public function __construct(TranslationInterface $string_translation, ModuleHandlerInterface $module_handler, PluginTypeManagerInterface $plugin_type_manager) {
-    $this->moduleHandler = $module_handler;
-    $this->pluginTypeManager = $plugin_type_manager;
-    $this->stringTranslation = $string_translation;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static($container->get('string_translation'), $container->get('module_handler'), $container->get('plugin.plugin_type_manager'));
-  }
+class ListPlugins extends ListBase {
 
   /**
    * Returns the route's title.
@@ -104,7 +62,7 @@ class ListPlugins extends ControllerBase {
         '#markup' => $plugin_definition_mapper->getPluginDescription($plugin_definition),
       ];
       $build[$plugin_id]['provider'] = [
-        '#markup' => $this->moduleHandler->getName($plugin_definition_mapper->getPluginProvider($plugin_definition)),
+        '#markup' => $this->getProviderLabel($plugin_definition_mapper->getPluginProvider($plugin_definition)),
       ];
     }
 
